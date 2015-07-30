68d6b5ad95043c9b8768677fe9d8fc589efab6fb
kkroner <kevin.m.kroner@gmail.com>
2015-07-30 14:48:14 -0400

    Add resharper shared rules.

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+  public class Class1
+  {
+    public Class1()
     {
+      var s = 3;
+      int y;
+
+
+
+
     }
+
+  }
+
+  public enum BadEnumNoItem
+  {
+    
+  }
 }
